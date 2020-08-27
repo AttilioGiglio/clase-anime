cb082252b95ada8f43e584574a31d2a83e55718f
Miguel Curilén <curilen.m@gmail.com>
2020-08-26 21:41:27 -0400

    add fetch anime

--- src/vista/Anime.js
+++ src/vista/Anime.js
@@ -1,19 +1,44 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { useParams, Link } from 'react-router-dom';
 
 const Anime = props => {
     const {id} = useParams();
-    console.log(id)
+    const [state, setState] = useState({
+      anime: null
+    })
+    console.log(id);
+
+    useEffect(() => {
+      fetch('https://kitsu.io/api/edge/anime/' + id)
+      .then((res) => res.json())
+      .then((data) => {
+        setState(s => ({ ...s, anime: data.data}))
+      })
+    }, [id])
 
     return (
-        <>
-            <div className="media">
-                <img src="..." className="mr-3" alt="..." />
-                <div className="media-body">
-                    <h5 className="mt-0">{id}</h5>
-                </div>
-            </div>
-        </>
+        <div>
+          <div className="col-6 offset-3">
+            <Link to="/">Volver a la lista</Link>
+          </div>
+          <div className="col-6 offset-3">
+          {
+            state.anime === null ? <div></div> : (
+              <div className="media">
+                  <img 
+                    style={{ maxWidth: '300px'}}
+                    src={state.anime.attributes.coverImage
+                      ? state.anime.attributes.coverImage.original
+                      : ""}
+                    className="mr-3" alt="..." />
+                  <div className="media-body">
+                      <h5 className="mt-0">{state.anime.attributes.synopsis }</h5>
+                  </div>
+              </div>
+            )
+          }
+          </div>
+        </div>
     )
 }
 
